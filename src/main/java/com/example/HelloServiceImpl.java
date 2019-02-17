dc3b710caeb0a711db2d6eb510ffb20efd6d04fe
JimDa <1085529137@qq.com>
2019-02-17 21:38:21 +0800

    init commit

---
+++ src/main/java/com/example/HelloServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.Date;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import org.springframework.web.servlet.support.RequestContext;
+
+@Service(version = "1.0.0")
+public class HelloServiceImpl implements HelloService {
+
+    @Override
+    public String sayHello(String name) {
+        LinkedBlockingDeque linkedBlockingDeque = new LinkedBlockingDeque();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 1, TimeUnit.HOURS, linkedBlockingDeque);
+        return "Hello, " + name + ", " + new Date();
+    }
+
+}
